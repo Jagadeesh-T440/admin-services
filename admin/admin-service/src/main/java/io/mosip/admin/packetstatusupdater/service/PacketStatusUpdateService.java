58118a2faf85deebc1ae0fac8395b9163fa8e526
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-15 13:29:00 +0530

    Update PacketStatusUpdateService.java

--- admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/PacketStatusUpdateService.java
+++ admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/service/PacketStatusUpdateService.java
@@ -2,6 +2,7 @@ package io.mosip.admin.packetstatusupdater.service;
 
 import io.mosip.admin.packetstatusupdater.dto.PacketResumeUpdateResponseDto;
 import io.mosip.admin.packetstatusupdater.dto.PacketStatusUpdateResponseDto;
+import io.mosip.admin.packetstatusupdater.dto.PacketSendToPersoResponseDto;
 
 /**
  * The Interface PacketStatusUpdateService.
@@ -20,4 +21,6 @@ public interface PacketStatusUpdateService {
 	public PacketStatusUpdateResponseDto getStatus(String rid, String langCode);
 	
 	public PacketResumeUpdateResponseDto updatePacket(String rid, String langCode);
+
+	public PacketSendToPersoResponseDto sentPacketCardToPerso(String rid, String langCode);
 }
