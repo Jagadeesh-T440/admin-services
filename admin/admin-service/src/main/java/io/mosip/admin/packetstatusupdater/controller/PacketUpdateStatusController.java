eabc2c8863227a24aa78ebcbed9e17f8ddd894af
Jagadeesh-T440 <jagadeesh.thiyagarajan@technoforte.co.in>
2025-12-16 11:18:49 +0530

    Update PacketUpdateStatusController.java

--- admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/controller/PacketUpdateStatusController.java
+++ admin/admin-service/src/main/java/io/mosip/admin/packetstatusupdater/controller/PacketUpdateStatusController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import io.mosip.admin.packetstatusupdater.dto.PacketSendToPersoResponseDto;
 import io.mosip.admin.packetstatusupdater.dto.PacketResumeUpdateResponseDto;
 import io.mosip.admin.packetstatusupdater.dto.PacketStatusUpdateResponseDto;
 import io.mosip.admin.packetstatusupdater.service.PacketStatusUpdateService;
@@ -70,4 +70,24 @@ public class PacketUpdateStatusController {
 	    return responseWrapper;
 		
 	}
+
+	/**
+	 * Validate packet.
+	 *
+	 * @param rId the r id
+	 * @return the response wrapper
+	 */
+	@PreAuthorize("hasAnyRole(@authorizedRoles.getGetpacketstatusupdate())")
+	@PostMapping(value = { "/sentToPerso" }, consumes = { "multipart/form-data" })
+	//@PreAuthorize("hasAnyRole('ZONAL_ADMIN','GLOBAL_ADMIN')")
+	public ResponseWrapper<PacketSendToPersoResponseDto> sentPacketCardToPerso(@RequestParam(value = "rid") String rId,
+			@RequestParam(value = "langCode", required = false) String langCode) {
+		auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.PKT_STATUS_UPD_API_CALLED, rId), null);
+	    ResponseWrapper<PacketSendToPersoResponseDto> responseWrapper = new ResponseWrapper<>();
+	    responseWrapper.setResponse(packetUpdateStatusService.sentPacketCardToPerso(rId, langCode));
+	    auditUtil.setAuditRequestDto(EventEnum.getEventEnumWithValue(EventEnum.PKT_STATUS_UPD_SUCCESS, rId), null);
+	    return responseWrapper;
+
+	}
 }
+
