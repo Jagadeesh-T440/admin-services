1c5b10013088d4edab51b3d565a840ad64fb029e
jagadeesh.t <jagathiyagu540@gmail.com>
2026-03-27 15:03:06 +0530

    updated download biosdk

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DownloadBioSdkZipController.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DownloadBioSdkZipController.java
@@ -1,12 +1,11 @@
 package io.mosip.kernel.masterdata.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import io.mosip.kernel.masterdata.service.FileDownloadService;
 
 import java.io.InputStream;
@@ -19,17 +18,28 @@ public class DownloadBioSdkZipController {
     private FileDownloadService fileDownloadService;
 
     @GetMapping("/bio-sdk")
-    public ResponseEntity<InputStreamResource> downloadFile() throws Exception {
+    public ResponseEntity<StreamingResponseBody> downloadFile() throws Exception {
 
         InputStream inputStream = fileDownloadService.downloadZip();
-        
         if (inputStream == null) {
             return ResponseEntity.notFound().build();
         }
-        InputStreamResource resource = new InputStreamResource(inputStream);
+
+        StreamingResponseBody stream = outputStream -> {
+            byte[] buffer = new byte[1024 * 1024];
+            int bytesRead;
+
+            try (inputStream) {
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                    outputStream.flush();
+                }
+            }
+        };
+
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=bio-sdk.zip")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+                .body(stream);
     }
 }
