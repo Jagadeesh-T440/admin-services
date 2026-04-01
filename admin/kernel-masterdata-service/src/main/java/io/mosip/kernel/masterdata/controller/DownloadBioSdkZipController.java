08cb54879ad0712e55750f1749ed0a3cf7fdc03d
jagadeesh.t <jagathiyagu540@gmail.com>
2026-04-01 17:48:39 +0530

    upd

--- admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DownloadBioSdkZipController.java
+++ admin/kernel-masterdata-service/src/main/java/io/mosip/kernel/masterdata/controller/DownloadBioSdkZipController.java
@@ -1,11 +1,11 @@
 package io.mosip.kernel.masterdata.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import io.mosip.kernel.masterdata.service.FileDownloadService;
@@ -22,15 +22,25 @@ public class DownloadBioSdkZipController {
     private FileDownloadService fileDownloadService;
 
     @GetMapping("/bio-sdk")
-    public ResponseEntity<InputStreamResource> downloadFile() throws Exception {
-    	logger.info("Received request to download Bio SDK zip file");
-    	
-    	InputStream inputStream = fileDownloadService.downloadZip();
-    	logger.info("Successfully Downloaded zip file from service");
-    	
+    public ResponseEntity<StreamingResponseBody> downloadFile() throws Exception {
+        logger.info("Received request to download Bio SDK zip file");
+
+        InputStream inputStream = fileDownloadService.downloadZip();
+
+        StreamingResponseBody stream = outputStream -> {
+            byte[] buffer = new byte[8192];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            inputStream.close();
+        };
+        
+        logger.info("Successfully Downloaded zip file from service...");
+
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=file.zip")
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(new InputStreamResource(inputStream));
+                .body(stream);
     }
 }
